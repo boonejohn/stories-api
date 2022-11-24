bfc27ef1d3512566e24543d9b314f17deaca10fa
John Boone <j.raditya.b@gmail.com>
2022-11-24 17:33:19 +0800

    9th

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -25,7 +25,6 @@ class Api::V1::StoriesController < Api::V1::BaseController
     end
   end
 
-
   private
 
   def set_story
@@ -37,7 +36,6 @@ class Api::V1::StoriesController < Api::V1::BaseController
   end
 
   def render_error
-    render json: { errors: @story.errors.full_messages },
-    status: :unprocessable_entity
+    render json: { errors: @story.errors.full_messages }, status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :stories, only: %i[index show create]
+      resources :stories, only: %i[index show create update]
     end
   end
 end
